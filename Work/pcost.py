c71dd28c6f79f1472244d380d397dabe837a784a
Ibrahim <ibrahim_9319@hotmail.com>
2020-08-23 16:22:18 +0100

    pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,14 +1,28 @@
+import csv
+import sys
 
-total = 0
-with open('Data/portfolio.csv', 'rt') as f: # read file line by line
-    headers = (next(f))
-    for line in f:
-        stock = line.split(',')
-        shares = int(stock[1])
-        price = float(stock[2])
-        total = total + (shares * price)
-        print(line, end='')
+def portfolio_cost(filename):
+    total = 0
+    f = open(filename)
+    rows = csv.reader(f)
+    headers = next(rows)
+    #    with open(filename, 'rt') as f: # read file line by line
+    #        headers = (next(f))
+    for row in rows:
+        try:
+                shares = int(row[1])
+                price = float(row[2])
+                total = total + (shares * price)
+        except ValueError:
+            print("Input file is invalid")
+    return total
 
-print(total)
+if (len(sys.argv)) == 2:
+    filename = sys.argv[1]  # read filename input from user
+else:
+    filename = 'Data/portfolio.csv' # default to this file if no input
+
+total = portfolio_cost(filename)
+print('Total cost: ', total)
 
 
